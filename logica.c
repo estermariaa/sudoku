acfdd17407c5e85a25a78f88115e6ceab44ed7aa
MateusoBrito <mateusdeoliveirabritoo@gmail.com>
2024-12-07 16:25:58 -0300

    solucao bruta

--- logica.c
+++ logica.c
@@ -40,39 +40,55 @@ void imprimirSudoku(char **matriz, int dimensao){
     }
 }
 
-int verificaLinha(char **matriz, int linha, int coluna,int dimensao){
-    int elemento = matriz[linha][coluna];
+int verificaLinha(char **matriz, int candidato, int linha,int dimensao){
     for(int i=0; i<dimensao; i++){
         // mudar para comparar com char
-        if(matriz[linha][i] == elemento && i != coluna){
+        if(matriz[linha][i] == candidato){
             return 0;
         }
     }
     return 1;
 }
 
-int verificaColuna(char **matriz, int linha, int coluna,int dimensao){
-    int elemento = matriz[linha][coluna];
+int verificaColuna(char **matriz, int candidato, int coluna,int dimensao){
     for(int i=0; i<dimensao; i++){
         // mudar para comparar com char
-        if(matriz[i][coluna] == elemento && i != coluna){
+        if(matriz[i][coluna] == candidato){
             return 0;
         }
     }
     return 1;
 }
 
-int verificaQuadrante(char **matriz, int linha, int coluna, int dimensao){
+int verificaQuadrante(char **matriz, int candidato, int linha, int coluna, int dimensao){
     int elemento = matriz[linha][coluna];
     int dim = sqrt(dimensao);
     int x0 = (linha/dim) * dim;
     int y0 = (coluna/dim) * dim;
     for(int i = 0; i<dim; i++){
         for(int j = 0; j<dim; j++){
-            if(matriz[x0+i][y0+j] == elemento && (x0+i)!=linha && (y0+j)!=coluna){
+            if(matriz[x0+i][y0+j] == candidato){
                 return 0;
             }
         }
     }
     return 1;
+}
+
+void solucao(char **matriz, int dimensao){
+    for(int x=0; x <dimensao; x++){
+        for(int y=0; y < dimensao; y++){
+            if(matriz[x][y] == 0){
+                for(int w=1; w < 10; w++){
+                    if(verificaLinha(matriz, w, x, dimensao) && verificaColuna(matriz, w, y, dimensao) && verificaQuadrante(matriz, w, x, y, dimensao)){
+                        // tem que mudar a logica para char
+                        matriz[x][y] = w;
+                        solucao(matriz, dimensao);
+                        matriz[x][y] = 0;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
